930ead8626015ee16cf4c38a780a3392a3043b28
ShimaaFawzy <eng.shimaafawzy@gmail.com>
2014-05-14 11:16:24 +0200

    update Beans

--- GuideMe/src/com/example/bean/Place.java
+++ GuideMe/src/com/example/bean/Place.java
@@ -1,12 +1,35 @@
 package com.example.bean;
 
+import com.google.android.gms.maps.model.Marker;
+
+import android.view.ViewGroup.MarginLayoutParams;
+
 public class Place {
 	
 	private int id;
-	private String placeName ,placeDescription , placeRate ,placeLongitude ,placeLatitude ,placeTicketPrice;
+	private String placeName ,placeDescription , placeRate ,placeLongitude ,placeLatitude ,placeTicketPrice , placeFromTo ,placeCategory;
+	private Marker marker;
+	
 	public int getId() {
 		return id;
 	}
+	
+	public String getPlaceFromTo() {
+		return placeFromTo;
+	}
+
+	public void setPlaceFromTo(String placeFromTo) {
+		this.placeFromTo = placeFromTo;
+	}
+
+	public String getPlaceCategory() {
+		return placeCategory;
+	}
+
+	public void setPlaceCategory(String placeCategory) {
+		this.placeCategory = placeCategory;
+	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
@@ -46,13 +69,24 @@ public class Place {
 	public void setPlaceTicketPrice(String placeTicketPrice) {
 		this.placeTicketPrice = placeTicketPrice;
 	}
+	
+	public Marker getMarker() {
+		return marker;
+	}
+
+	public void setMarker(Marker marker) {
+		this.marker = marker;
+	}
+
 	@Override
 	public String toString() {
 		return "Place [id=" + id + ", placeName=" + placeName
 				+ ", placeDescription=" + placeDescription + ", placeRate="
 				+ placeRate + ", placeLongitude=" + placeLongitude
 				+ ", placeLatitude=" + placeLatitude + ", placeTicketPrice="
-				+ placeTicketPrice + "]";
+				+ placeTicketPrice + ", placeFromTo=" + placeFromTo
+				+ ", placeCategory=" + placeCategory + ", marker=" + marker
+				+ "]";
 	}
 	
 	
